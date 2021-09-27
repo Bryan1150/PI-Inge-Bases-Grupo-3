07f8c7ba9399eab02443547a8b669820844b2109
Diego Diaz <dddxfonseca@gmail.com>
2021-09-26 21:36:06 -0600

    Archivó para el handler de funcionarios fue creado

---
+++ Planetario/Planetario/Handlers/FuncionariosHandler.cs
@@ -0,0 +1,17 @@
+﻿using Planetario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Planetario.Handlers
+{
+    public class FuncionariosHandler
+    {
+        
+
+    }
+}
