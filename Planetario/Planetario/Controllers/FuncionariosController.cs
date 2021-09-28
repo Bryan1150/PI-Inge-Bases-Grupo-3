345cb44cd7b376725b5fc841ce15dc179f286eb5
Diego Diaz <dddxfonseca@gmail.com>
2021-09-28 02:30:36 -0600

    Creación archivo para el controlador

---
+++ Planetario/Planetario/Controllers/FuncionariosController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Planetario.Handlers;
+
+namespace Planetario.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        
+    }
+}
