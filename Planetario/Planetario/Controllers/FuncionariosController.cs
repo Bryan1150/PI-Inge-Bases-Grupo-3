b757a386bb7dca9020a012771184807b9dc5c835
Diego Diaz <dddxfonseca@gmail.com>
2021-09-28 02:11:12 -0600

    Modificaciones al Handler. Debido a cambios en la base de datos

---
+++ Planetario/Planetario/Controllers/FuncionariosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Planetario.Models;
+using Planetario.Handlers;
+
+namespace Planetario.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        public ActionResult ListaFuncionarios()
+        {
+            FuncionariosHandler AccesoDatos = new FuncionariosHandler();
+            ViewBag.listaFuncionarios = AccesoDatos.ObtenerTodosLosFuncionarios();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ObtenerImagen(int cedula)
+        {
+            FuncionariosHandler AccesoDatos = new FuncionariosHandler();
+            var tuple = AccesoDatos.ObtenerFoto(cedula);
+            return File(tuple.Item1, tuple.Item2);
+        }
+
+    }
+}
