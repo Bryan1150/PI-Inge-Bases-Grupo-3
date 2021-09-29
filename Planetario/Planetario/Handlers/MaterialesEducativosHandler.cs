bfc3f5a6594af6540517e8a223a5804ac870d9b9
Diego Diaz <dddxfonseca@gmail.com>
2021-09-29 16:39:08 -0600

    Función del handler para obtener los bytes de un documento

--- Planetario/Planetario/Handlers/MaterialesEducativosHandler.cs
+++ Planetario/Planetario/Handlers/MaterialesEducativosHandler.cs
@@ -4,6 +4,7 @@ using System.Web;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 
 using Planetario.Models;
 
@@ -20,64 +21,15 @@ namespace Planetario.Handlers
             Conexion = new SqlConnection(RutaConexion);
         }
 
-        private DataTable CrearTablaConsulta(string consulta)
-        {
-            SqlCommand comandoParaConsulta = new SqlCommand(consulta, Conexion);
-            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
-            DataTable consultaFormatoTabla = new DataTable();
-
-            Conexion.Open();
-            adaptadorParaTabla.Fill(consultaFormatoTabla);
-            Conexion.Close();
-            return consultaFormatoTabla;
-        }
-
-        public List<MaterialEducativoModel> ObtenerInfoTodosLosMateriales()
-        {
-            List<MaterialEducativoModel> ListaMateriales = new List<MaterialEducativoModel>();
-            string Consulta = "SELECT M.idMaterialPk, M.titulo, CONVERT(VARCHAR(20), M.fechaSubida, 1) AS fechaSubida , M.correoResponsableFK, " +
-                "M.idioma, M.autor, U.nombre + ' ' + U.apellido1 + ' ' + U.apellido2 AS 'nombreResponsable' " +
-                "FROM MaterialEducativo M " +
-                "JOIN Usuario U ON M.correoResponsableFK = U.correoPK;";
-            DataTable tablaResultado = CrearTablaConsulta(Consulta);
-
-            foreach (DataRow columna in tablaResultado.Rows)
-            {
-                ListaMateriales.Add(
-                    new MaterialEducativoModel
-                    {
-                        Id = Convert.ToInt32(columna["idMaterial"]),
-                        Titulo = Convert.ToString(columna["titulo"]),
-                        Fecha  = Convert.ToString(columna["fechaSubida"]),
-                        CorreoResponsable = Convert.ToString(columna["correoResponsable"]),
-                        Idioma = Convert.ToString(columna["idioma"]),
-                        Autor = Convert.ToString(columna["autor"]),
-                        NombreResponsable = Convert.ToString(columna["nombreResponsable"]),
-                    }
-                );
-            }
-
-            return ListaMateriales;
-        }
-
-        public Tuple<byte[], string> ObtenerVistaPrevia(int id)
+        private byte[] ObtenerBytes(HttpPostedFileBase archivo)
         {
             byte[] bytes;
-            string contentType;
-            string consulta = "SELECT imagenVistaPrevia, tipoArchivoVistaPrevia FROM MaterialEducativo WHERE idMaterialPK = @id";
-            SqlCommand comandoParaConsulta = new SqlCommand(consulta, Conexion);
-            comandoParaConsulta.Parameters.AddWithValue("@id", id);
-            Conexion.Open();
-
-            SqlDataReader lectorDeDatos = comandoParaConsulta.ExecuteReader();
-            lectorDeDatos.Read();
-
-            bytes = (byte[])lectorDeDatos["foto"];
-            contentType = lectorDeDatos["tipoArchivoFoto"].ToString();
-
-            Conexion.Close();
-            return new Tuple<byte[], string>(bytes, contentType);
+            BinaryReader lector = new BinaryReader(archivo.InputStream);
+            bytes = lector.ReadBytes(archivo.ContentLength);
+            return bytes;
         }
+
+        
 
     }
 }
