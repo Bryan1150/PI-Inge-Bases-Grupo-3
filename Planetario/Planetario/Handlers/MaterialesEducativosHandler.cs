dfe73c718d177b42d22e5ebf57e16e5110379ce1
Diego Diaz <dddxfonseca@gmail.com>
2021-09-29 02:11:43 -0600

    Le cambié el nombre a la clase

--- Planetario/Planetario/Handlers/MaterialesEducativosHandler.cs
+++ Planetario/Planetario/Handlers/MaterialesEducativosHandler.cs
@@ -5,6 +5,8 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 
+using Planetario.Models;
+
 namespace Planetario.Handlers
 {
     public class MaterialesEducativosHandler
@@ -18,7 +20,39 @@ namespace Planetario.Handlers
             Conexion = new SqlConnection(RutaConexion);
         }
 
-        
+        private DataTable CrearTablaConsulta(string consulta)
+        {
+            SqlCommand comandoParaConsulta = new SqlCommand(consulta, Conexion);
+            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+
+            Conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            Conexion.Close();
+            return consultaFormatoTabla;
+        }
+
+        public List<MaterialEducativoModel> ObtenerInfoTodosLosMateriales()
+        {
+            List<MaterialEducativoModel> ListaMateriales = new List<MaterialEducativoModel>();
+            string Consulta = "SELECT M.titulo, CONVERT(VARCHAR(20), M.fechaSubida, 1) AS fechaSubida , M.correoResponsableFK, " +
+                "M.idioma, M.autor, U.nombre + ' ' + U.apellido1 + ' ' + U.apellido2 AS 'nombreResponsable' " +
+                "FROM MaterialEducativo M " +
+                "JOIN Usuario U ON M.correoResponsableFK = U.correoPK;";
+            DataTable tablaResultado = CrearTablaConsulta(Consulta);
+
+            foreach (DataRow columna in tablaResultado.Rows)
+            {
+                ListaMateriales.Add(
+                    new MaterialEducativoModel
+                    {
+                        Titulo = 
+                    }
+                );
+            }
+
+            return ListaMateriales;
+        }
 
     }
 }
--- Planetario/Planetario/Models/MaterialEducativoModel.cs
+++ Planetario/Planetario/Models/MaterialEducativoModel.cs
@@ -3,27 +3,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Planetario.Models
 {
-    public class MaterialesEducativosModel
+    public class MaterialEducativoModel
     {
         [Display(Name = "Titulo")]
         [Required(ErrorMessage = "Es necesario indicar un titulo para el material")]
-        private string Titulo { get; set; }
+        public string Titulo { get; set; }
         
         [Display(Name = "Fecha")]
-        private string Fecha { get; set; }
+        public string Fecha { get; set; }
 
         [Display(Name = "Correo del responsable")]
         [Required(ErrorMessage = "Es necesario indicar le correo del usuario")]
-        private string CorreoResponsable { get; set; }
+        public string CorreoResponsable { get; set; }
 
         [Display(Name = "Idioma")]
-        private string Idioma { get; set; }
+        public string Idioma { get; set; }
         
         [Display(Name = "Autor")]
-        private string Autor { get; set; }
+        public string Autor { get; set; }
 
         [Display(Name = "Nombre del responsable")]
-        private string NombreResponsable { get; set; }
+        public string NombreResponsable { get; set; }
 
         [Display(Name = "Imagen de vista previa")]
         public HttpPostedFileBase ImagenVistaPrevia { get; set; }
