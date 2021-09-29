ffe87d969cb34d67cdea8b02797052e7a69592fd
Bryan1150 <bryan.umaa1150@gmail.com>
2021-09-28 20:22:33 -0600

    Handler de Preguntas Frecuentes Base

---
+++ Planetario/Planetario/Handlers/PreguntasFrecuentesHandler.cs
@@ -0,0 +1,59 @@
+﻿using Planetario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Planetario.Handlers
+{
+    public class PreguntasFrecuentesHandler
+    {
+        private SqlConnection conexion;
+        private readonly string rutaConexion;
+
+        public PreguntasFrecuentesHandler()
+        {
+            rutaConexion = ConfigurationManager.ConnectionStrings["PlanetarioConnection"].ToString();
+            conexion = new SqlConnection(rutaConexion);
+        }
+
+        private DataTable CrearTablaConsulta(string consulta)
+        {
+            SqlCommand comandoParaConsulta = new SqlCommand(consulta, conexion);
+            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+
+            conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            conexion.Close();
+            return consultaFormatoTabla;
+        }
+
+        public List<PreguntasFrecuentesModel> ObtenerPreguntasFrecuentes()
+        {
+            List<PreguntasFrecuentesModel> preguntasFrecuentes = new List<PreguntasFrecuentesModel>();
+            string consulta = "SELECT * FROM dbo.PreguntasFrecuentes";
+            DataTable tablaResultado = CrearTablaConsulta(consulta);
+
+            foreach (DataRow columna in tablaResultado.Rows)
+            {
+                preguntasFrecuentes.Add(
+                    new PreguntasFrecuentesModel
+                    {
+                        idPregunta = Convert.ToInt32(columna["idPreguntaPK"]),
+                        categoriaPregunta = Convert.ToString(columna["categoriaPregunta"]),
+                        topicoPregunta = Convert.ToString(columna["topicoPregunta"]),
+                        pregunta = Convert.ToString(columna["pregunta"]),
+                        respuesta = Convert.ToString(columna["respuesta"]),
+                    });           
+            }
+
+            return preguntasFrecuentes;
+        }
+    }   
+}
+
+    
---
+++ Planetario/Planetario/Models/PreguntasFrecuentesModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Planetario.Models
+{
+    public class PreguntasFrecuentesModel
+    {
+        public int idPregunta { get; set; }
+
+        [Display(Name = "Ingrese la categoria de la pregunta")]
+        [Required(ErrorMessage = "Es necesario que ingrese la categoria de la pregunta")]
+        [MaxLength(100, ErrorMessage = "Se tiene un máximo de 100 cáracteres")]
+        public string categoriaPregunta { get; set; }
+
+        [Display(Name = "Ingrese el topico de la pregunta")]
+        [Required(ErrorMessage = "Es necesario que ingrese el topico de la pregunta")]
+        [MaxLength(100, ErrorMessage = "Se tiene un máximo de 100 cáracteres")]
+        public string topicoPregunta { get; set; }
+
+        [Display(Name = "Ingrese la pregunta")]
+        [Required(ErrorMessage = "Es necesario que ingrese la pregunta")]
+        public string pregunta { get; set; }
+
+        [Display(Name = "Ingrese la respuesta")]
+        [Required(ErrorMessage = "Es necesario que ingrese la respuesta")]
+        public string respuesta { get; set; }
+    }
+}
