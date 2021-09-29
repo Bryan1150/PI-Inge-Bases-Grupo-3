45877062b105db253251c8233b44be60f978c23c
Diego Diaz <dddxfonseca@gmail.com>
2021-09-29 16:55:55 -0600

    Función para recibir la información ingresada en el formulario

--- Planetario/Planetario/Controllers/MaterialEducativoController.cs
+++ Planetario/Planetario/Controllers/MaterialEducativoController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 
 using Planetario.Handlers;
+using Planetario.Models;
 
 namespace Planetario.Controllers
 {
@@ -13,6 +14,31 @@ namespace Planetario.Controllers
         public ActionResult AlmacenarNuevoMaterialEducativo()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AlmacenarNuevoMaterialEducativo(MaterialEducativoModel Material)
+        {
+            ViewBag.ExitoAlmacenar = false;
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    MaterialesEducativosHandler AccesoADatos = new MaterialesEducativosHandler();
+                    ViewBag.ExitoAlmacenar = AccesoADatos.AlmacenarMaterialEducativo(Material);
+                    if(ViewBag.ExitoAlmacenar)
+                    {
+                        ViewBag.Mensaje = "El material " + Material.Titulo + "fue almacenado con exito 😉";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Messasge = "Algo salió mal y no fue posible crear el planeta 😐";
+                return View();
+            }
         }
 
     }
