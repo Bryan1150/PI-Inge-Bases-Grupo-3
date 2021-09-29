39887159c7ea2c71991899613d3caf8a86427e06
Diego Diaz <dddxfonseca@gmail.com>
2021-09-29 01:48:06 -0600

    Elimine importaciones innecesarias

--- Planetario/Planetario/Models/MaterialesEducativosModel.cs
+++ Planetario/Planetario/Models/MaterialesEducativosModel.cs
@@ -1,13 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.ComponentModel.DataAnnotations;
 
 namespace Planetario.Models
 {
     public class MaterialesEducativosModel
     {
+        [Display(Name = "Titulo")]
+        [Required(ErrorMessage = "Es necesario indicar un titulo para el material")]
+        private string Titulo { get; set; }
+        
+        [Display(Name = "Fecha")]
+        private string Fecha { get; set; }
+
+        [Display(Name = "Correo del responsable")]
+        [Required(ErrorMessage = "Es necesario indicar le correo del usuario")]
+        private string CorreoResponsable { get; set; }
+
+        [Display(Name = "Idioma")]
+        private string Idioma { get; set; }
+        
+        [Display(Name = "Autor")]
+        private string Autor { get; set; }
+
+        [Display(Name = "Nombre del responsable")]
+        private string NombreResponsable { get; set; }
+
+        [Display(Name = "Imagen de vista previa")]
+        public HttpPostedFileBase ImagenVistaPrevia { get; set; }
+
+        public string TipoImagenVistaPrevia { get; set; }
+
+        [Display(Name = "Archivo")]
+        [Required(ErrorMessage = "Es necesario subir un archivo")]
+        public HttpPostedFileBase Foto { get; set; }
+
+        public string TipoArchivo { get; set; }
 
     }
 }
