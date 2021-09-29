6efa5ae1d0a866a2a71057e8e1c468f1bbfb10ab
Diego Diaz <dddxfonseca@gmail.com>
2021-09-29 02:01:05 -0600

    Creación del arhivo para para el handler de los materiales educativos

---
+++ Planetario/Planetario/Handlers/MaterialesEducativosHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Planetario.Handlers
+{
+    public class MaterialesEducativosHandler
+    {
+        
+
+    }
+}
