0e2f940e78bc556c2f76ed990ce113dd0530872f
Diego Diaz <dddxfonseca@gmail.com>
2021-10-27 09:42:55 -0600

    Constructor para la el hanlder de la base de datos

--- Planetario/Planetario/Handlers/BaseDatosHandler.cs
+++ Planetario/Planetario/Handlers/BaseDatosHandler.cs
@@ -1,10 +1,21 @@
-﻿using System.Configuration;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace Planetario.Handlers
 {
     public class BaseDatosHandler
     {
+        public SqlConnection Conexion { get; }
+        private readonly string RutaConexion;
+
+        public BaseDatosHandler()
+        {
+            RutaConexion = ConfigurationManager.ConnectionStrings["ConexionBaseDatosServidor"].ToString();
+            Conexion = new SqlConnection(RutaConexion);
+        }
+
         
 
     }
