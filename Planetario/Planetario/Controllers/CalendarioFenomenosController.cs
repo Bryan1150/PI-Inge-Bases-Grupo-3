8cb2a8687b062fbb695a3ebc3821b62f9215bb94
Bryan1150 <bryan.umaa1150@gmail.com>
2021-11-01 23:58:33 -0600

    Solo falta interconexion entre datos del controller con el script

--- Planetario/Planetario/Controllers/CalendarioFenomenosController.cs
+++ Planetario/Planetario/Controllers/CalendarioFenomenosController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Planetario.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace Planetario.Controllers
 {
@@ -11,9 +13,32 @@ namespace Planetario.Controllers
         // GET: Calendario
         public ActionResult CalendarioFenomenos()
         {
-            //CalendarioFenomenosHandler accesoDatos = new PreguntasFrecuentesHandler();
-            //ViewBag.preguntasFrecuentes = accesoDatos.ObtenerPreguntasFrecuentes();
+            XDocument xml = XDocument.Load("https://in-the-sky.org//rss.php?feed=dfan&latitude=9.93333&longitude=-84.08333&timezone=America/Costa_Rica");
+            var RSSFeedData = (from x in xml.Descendants("item")
+                               select new RSSFeedModel
+                               {
+                                   Title = ((string)x.Element("title")),
+                                   Link = ((string)x.Element("link")),
+                                   Description = ((string)x.Element("description")),
+                                   PubDate = ((string)x.Element("pubDate"))
+                               });
+            ViewBag.RSSFeed = RSSFeedData;         
             return View();
+        }
+
+        public JsonResult GetEventsForCalendar()
+        {
+            XDocument xml = XDocument.Load("https://in-the-sky.org//rss.php?feed=dfan&latitude=9.93333&longitude=-84.08333&timezone=America/Costa_Rica");
+            var RSSFeedData = (from x in xml.Descendants("item")
+                               select new RSSFeedModel
+                               {
+                                   Title = ((string)x.Element("title")),
+                                   Link = ((string)x.Element("link")),
+                                   Description = ((string)x.Element("description")),
+                                   PubDate = ((string)x.Element("pubDate"))
+                               });
+           
+            return Json(RSSFeedData, JsonRequestBehavior.AllowGet);
         }
     }
 }
