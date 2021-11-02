7b94397f402a291ce821a43403544d0cc9f96c9a
Bryan1150 <bryan.umaa1150@gmail.com>
2021-11-01 18:55:07 -0600

    Controlador Calendario

---
+++ Planetario/Planetario/Controllers/CalendarioFenomenosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planetario.Controllers
+{
+    public class CalendarioController : Controller
+    {
+        // GET: Calendario
+        public ActionResult CalendarioFenomenos()
+        {
+            //CalendarioFenomenosHandler accesoDatos = new PreguntasFrecuentesHandler();
+            //ViewBag.preguntasFrecuentes = accesoDatos.ObtenerPreguntasFrecuentes();
+            return View();
+        }
+    }
+}
