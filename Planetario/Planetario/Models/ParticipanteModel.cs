c7a144e0cec78d520bfd81ba33ae986f179934a6
Diego Diaz <dddxfonseca@gmail.com>
2021-11-01 22:33:44 -0600

    Modelo para el participante

--- Planetario/Planetario/Models/ParticipanteModel.cs
+++ Planetario/Planetario/Models/ParticipanteModel.cs
@@ -7,5 +7,22 @@ namespace Planetario.Models
 {
     public class ParticipanteModel
     {
+        public string Correo { get; set; }
+
+        public string Nombre { get; set; }
+        
+        public string Apellido1 { get; set; }
+        
+        public string Apellido2 { get; set; }
+        
+        public string Genero { get; set; }
+        
+        public string Pais { get; set; }
+        
+        public string FechaNacimiento { get; set; }
+        
+        public string NivelEducativo { get; set; }
+        
+        public string NombreActividad { get; set; }
     }
 }
