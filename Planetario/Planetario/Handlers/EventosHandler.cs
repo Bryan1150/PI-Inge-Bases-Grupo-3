90c5fec0088d29ff90bd3e12453478eed8dd58c2
Diego Diaz <dddxfonseca@gmail.com>
2021-11-04 19:04:34 -0600

    Creación del hanlder para eventos

---
+++ Planetario/Planetario/Handlers/EventosHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using Planetario.Models;
+
+namespace Planetario.Handlers
+{
+    public class EventosHandler: BaseDatosHandler
+    {
+        public bool InsertarActividad(EventoModel evento)
+        {
+            bool exito;
+            string Consulta = "INSERT INTO Eventos ( titulo, fecha, descripcion ) "
+                + "VALUES ( @titulo, @fecha, @descripcion );";
+
+            Dictionary<string, object> valoresParametros = new Dictionary<string, object> {
+                {"@titulo", evento.Titulo },
+                {"@fecha", evento.Fecha },
+                {"@descripcion", evento.Descripcion }
+            };
+
+            exito = InsertarEnBaseDatos(Consulta, valoresParametros);
+
+            return exito;
+        }
+
+    }
+}
