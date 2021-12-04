4693743d4991948af35b3a338aab7c6cb2674c92
Edu F <efernandezbaldi@gmail.com>
2021-12-04 15:16:52 -0600

    modificar un poco el modal

--- Planetario/Planetario/Utils/js/verActividad.js
+++ Planetario/Planetario/Utils/js/verActividad.js
@@ -4,12 +4,12 @@
 }
 function takeData() {
     if (($("#Username").val().length == 0) || ($("#Numseats").val().length == 0)) {
-        alert("Please Enter your Name and Number of Seats");
+        alert("Por favor ingrese el número de entradas");
     }
     else {
         $(".inputForm *").prop("disabled", true);
         $(".seatStructure *").prop("disabled", false);
-        document.getElementById("notification").innerHTML = "<b style='margin-bottom:0px;background:yellow;'>Please Select your Seats NOW!</b>";
+        document.getElementById("notification").innerHTML = "<b style='margin-bottom:0px;background:yellow;'>Escoja sus asientos</b>";
     }
 }
 
@@ -36,7 +36,7 @@ function updateTextArea() {
         $('#seatsDisplay').val(allSeatsVals);
     }
     else {
-        alert("Please select " + ($("#Numseats").val()) + " seats")
+        alert("Por favor escoja " + ($("#Numseats").val()) + " asientos")
     }
 }
 
