6a371a461299daf65680bb8a328633cb1d53e653
asdru333 <asdrubal.villegasm@gmail.com>
2021-12-06 18:38:33 -0600

    handler y modelo de descuento agregado

---
+++ Planetario/Planetario/Models/DescuentoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planetario.Models
+{
+    public class DescuentoModel
+    {
+        [Required(ErrorMessage = "Es necesario que indique el codigo de descuento")]
+        [Display(Name = "Codigo")]
+        [MaxLength(10, ErrorMessage = "Se tiene un máximo de 10 cáracteres")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que indique la cantidad de descuento")]
+        [Display(Name = "Descuento")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Debe ingresar números")]
+        public int Descuento { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que indique el tipo de membresia")]
+        [Display(Name = "Membresia")]
+        public string Membresia { get; set; }
+    }
+}
