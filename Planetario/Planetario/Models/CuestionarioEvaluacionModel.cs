85a41319564d47fe3b7f003d24ac94b70d416a71
asdru333 <asdrubal.villegasm@gmail.com>
2021-12-07 15:16:45 -0600

    modelos  y handler de cuestionarioEvaluacionRecibir

---
+++ Planetario/Planetario/Models/CuestionarioEvaluacionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Planetario.Models
+{
+    public class CuestionarioEvalucionModel
+    {
+        [Display(Name = "Nombre del cuestionario")]
+        [Required(ErrorMessage = "Es necesario indicar el nombre del cuestionario.")]
+        public string NombreCuestionario { get; set; }
+
+        [Display(Name = "Categoria del cuestionario")]
+        [Required(ErrorMessage = "Es necesario que ingrese la categoria del cuestionario.")]
+        public string Categoria { get; set; }
+    }
+}
---
+++ Planetario/Planetario/Models/CuestionarioEvaluacionRecibirModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Planetario.Models
+{
+    public class CuestionarioEvaluacionRecibirModel : CuestionarioEvalucionModel
+    {
+
+        public List<string> Preguntas { get; set; }
+
+        List<string> Respuestas { get; set; }
+
+        List<string> Comentario { get; set; }
+
+        public CuestionarioEvaluacionRecibirModel()
+        {
+            Preguntas = new List<string>();
+            Respuestas = new List<string>();
+        }
+    }
+}
