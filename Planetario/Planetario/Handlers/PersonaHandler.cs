0bf97a7c899275ee0d2764a18857baf5392e15ba
danielmonge25 <danielmonge25@hotmail.com>
2021-12-09 17:43:13 -0600

    Implementación de comprar membresias y actualizacion en base de datos

--- Planetario/Planetario/Handlers/PersonaHandler.cs
+++ Planetario/Planetario/Handlers/PersonaHandler.cs
@@ -47,28 +47,5 @@ namespace Planetario.Handlers
             }
             return tipoUsuario;
         }
-
-        public string ObtenerMembresia(string correo) 
-        {
-            string consultaTablaPersona = "SELECT membresia " +
-                                          "FROM Persona " +
-                                          "WHERE correoPersonaPK = '" + correo + "' ";
-
-            DataTable tabla = LeerBaseDeDatos(consultaTablaPersona);
-            DataRow columna = tabla.Rows[0];
-            string membresia = Convert.ToString(columna["membresia"]);
-
-            return membresia;
-        }
-
-        public bool ActualizarMembresia(string correo, string membresia)
-        {
-            string consultaTablaPersona = "UPDATE Persona " +
-                                          "SET membresia = '" + membresia + "', " +
-                                          "compraMembresia = GETDATE() " +
-                                          "WHERE correoPersonaPK = '" + correo + "' ";
-
-            return (ActualizarEnBaseDatos(consultaTablaPersona, null));
-        }
     }
 }
