c7ca46b8c8ed2e5c9c1c59fec987a4edd48e5373
Diego Diaz <dddxfonseca@gmail.com>
2021-12-08 20:31:09 -0600

    Le añadí las funcionalidades a la evaluación

--- Planetario/Planetario/Models/CuestionarioEvaluacionRecibirModel.cs
+++ Planetario/Planetario/Models/CuestionarioEvaluacionRecibirModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Planetario.Models
 {
@@ -16,5 +17,9 @@ namespace Planetario.Models
             Preguntas = new List<string>();
             Respuestas = new List<string>();
         }
+
+        [Required(ErrorMessage = "Es necesario que escoje al menos una funcionalidad.")]
+        [Display(Name = "Funcionalidades")]
+        public string Funcionalidades { get; set; }
     }
 }
