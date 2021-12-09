ba360401465c9952a55c2a978af23a7e148d4703
danielmonge25 <danielmonge25@hotmail.com>
2021-12-08 19:26:40 -0600

    Controlador y Handler de Registro y Manejo de Membresias

--- Planetario/Planetario/Handlers/PersonaHandler.cs
+++ Planetario/Planetario/Handlers/PersonaHandler.cs
@@ -47,5 +47,56 @@ namespace Planetario.Handlers
             }
             return tipoUsuario;
         }
+
+        public bool InsertarUsuario(PersonaModel persona)
+        {
+            string consultaTablaPersona = "INSERT INTO Persona ( correoPersonaPK, nombre, apellido1, apellido2, genero, pais, fechaNacimiento, membresia ) "
+                + "VALUES ( @correo, @nombre, @apellido1, @apellido2, @genero, @pais, @nacimiento, @membresia );";
+
+            string consultaTablaCliente = "INSERT INTO Cliente ( correoClienteFK, nivelEducativo ) "
+                + "VALUES ( @correo, @nivelEducativo );";
+
+            Dictionary<string, object> parametrosPersona = new Dictionary<string, object> {
+                {"@correo", persona.correo },
+                {"@nombre", persona.nombre },
+                {"@apellido1", persona.apellido1 },
+                {"@apellido2", persona.apellido2 },
+                {"@genero", persona.genero },
+                {"@pais", persona.pais },
+                {"@nacimiento", persona.fechaNacimiento},
+                {"@membresia", "Terrestre" }
+            };
+
+            Dictionary<string, object> parametrosCliente = new Dictionary<string, object>
+            {
+                {"@correo", persona.correo },
+                {"@nivelEducativo", persona.nivelEducativo },
+            };
+
+            return (InsertarEnBaseDatos(consultaTablaPersona, parametrosPersona) && InsertarEnBaseDatos(consultaTablaCliente, parametrosCliente));
+        }
+
+        public string ObtenerMembresia(string correo) 
+        {
+            string consultaTablaPersona = "SELECT membresia " +
+                                          "FROM Persona " +
+                                          "WHERE correoPersonaPK = '" + correo + "' ";
+
+            DataTable tabla = LeerBaseDeDatos(consultaTablaPersona);
+            DataRow columna = tabla.Rows[0];
+            string membresia = Convert.ToString(columna["membresia"]);
+
+            return membresia;
+        }
+
+        public bool ActualizarMembresia(string correo, string membresia)
+        {
+            string consultaTablaPersona = "UPDATE Persona " +
+                                          "SET membresia = '" + membresia + "', " +
+                                          "compraMembresia = GETDATE() " +
+                                          "WHERE correoPersonaPK = '" + correo + "' ";
+
+            return (ActualizarEnBaseDatos(consultaTablaPersona, null));
+        }
     }
 }
