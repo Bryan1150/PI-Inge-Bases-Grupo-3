72434770d07949f8752e41882de6bf38ea2469ae
Edu F <efernandezbaldi@gmail.com>
2021-12-10 18:38:47 -0600

    ortografia y corregir warnings

--- Planetario/Planetario/Models/DescuentoModel.cs
+++ Planetario/Planetario/Models/DescuentoModel.cs
@@ -11,17 +11,17 @@ namespace Planetario.Models
     public class DescuentoModel
     {
         [Required(ErrorMessage = "Es necesario que indique el codigo de descuento")]
-        [Display(Name = "Codigo")]
+        [Display(Name = "Código")]
         [MaxLength(10, ErrorMessage = "Se tiene un máximo de 10 cáracteres")]
         public string Codigo { get; set; }
 
-        [Required(ErrorMessage = "Es necesario que indique la cantidad de descuento")]
-        [Display(Name = "Descuento")]
+        [Required(ErrorMessage = "Es necesario que indique el porcentaje de descuento")]
+        [Display(Name = "Porcentaje")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "Debe ingresar números")]
         public int Descuento { get; set; }
 
-        [Required(ErrorMessage = "Es necesario que indique el tipo de membresia")]
-        [Display(Name = "Membresia")]
+        [Required(ErrorMessage = "Es necesario que indique el tipo de membresía")]
+        [Display(Name = "Membresía")]
         public string Membresia { get; set; }
     }
 }
