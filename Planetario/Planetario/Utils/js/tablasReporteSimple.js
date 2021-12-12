085d9406f224d7209ae4e164ca503b4aeb25800e
Edu F <efernandezbaldi@gmail.com>
2021-12-12 12:02:11 -0600

    ortografia reporte simple

--- Planetario/Planetario/Utils/js/tablasReporteSimple.js
+++ Planetario/Planetario/Utils/js/tablasReporteSimple.js
@@ -8,8 +8,8 @@ function mostrarTablaRanking() {
     const columnas = [
         { id: "Nombre",   name: "Nombre" },
         { id: "Precio", name: "Precio(₡)" },
-        { id: "fechaIngreso", name: "fechaIngreso" },
-        { id: "fechaUltimaVenta", name: "fechaUltimaVenta" },
+        { id: "fechaIngreso", name: "Fecha de ingreso" },
+        { id: "fechaUltimaVenta", name: "Fecha de última venta" },
     ]
     const contenedor = document.getElementById("tablaProductosConFiltro")
     mostrarTabla(gridRanking, columnas, contenedor, actualizarTablaPorRanking)
