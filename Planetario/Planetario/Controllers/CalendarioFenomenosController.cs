560e29f957f37e9346a6dfbad195cf5e3e69e11c
Bryan1150 <bryan.umaa1150@gmail.com>
2021-12-12 19:20:36 -0600

    Eliminados todos los using que no estabamos usando

--- Planetario/Planetario/Controllers/CalendarioFenomenosController.cs
+++ Planetario/Planetario/Controllers/CalendarioFenomenosController.cs
@@ -3,7 +3,6 @@ using Planetario.Handlers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Xml.Linq;
 
--- Planetario/Planetario/Handlers/MembresiasHandler.cs
+++ Planetario/Planetario/Handlers/MembresiasHandler.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using Planetario.Models;
 using Planetario.Interfaces;
 
 namespace Planetario.Handlers
